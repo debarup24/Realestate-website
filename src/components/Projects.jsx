fbd3915bb5be0087e4fddf764613ce3150d9a9f8
Debarup <87480898+debarup24@users.noreply.github.com>
2024-11-24 19:37:18 +0530

    project slider

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { assets, projectsData } from "../assets/assets";
 
 const Projects = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardsToShow, setCardsToShow] = useState(1);
+
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length);
+  };
+
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className=" container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden">
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
@@ -17,6 +30,7 @@ const Projects = () => {
       {/* <-- Slider Button --> */}
       <div className="flex justify-end items-center mb-8">
         <button
+          onClick={prevProject}
           className="bg-gray-200 rounded mr-2 p-3"
           aria-label="Previous Project"
         >
@@ -24,6 +38,7 @@ const Projects = () => {
         </button>
 
         <button
+          onClick={nextProject}
           className="bg-gray-200 rounded mr-2 p-3"
           aria-label="Next Project"
         >
@@ -33,17 +48,32 @@ const Projects = () => {
 
       {/* <-- Project Slider Container -->  */}
       <div className="overflow-hidden ">
-        <div className="flex gap-8 transition-transform duration-500 ease-in-out">
+        <div
+          className="flex gap-8 transition-transform duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${(currentIndex * 100) / cardsToShow}%)`,
+          }}
+        >
           {projectsData.map((project, index) => (
             <div
               key={index}
               className=" relative flex-shrink-0 w-full sm:w-1/4"
             >
               <img
-                className=" rounded-xl "
+                className=" rounded-xl w-full h-auto mb-14 "
                 src={project.image}
                 alt={project.title}
               />
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center ">
+                <div className=" inline-block bg-white w-3/4 px-4 py-2 shadow-md">
+                  <h2 className="text-xl font-semibold text-gray-800 ">
+                    {project.title}
+                  </h2>
+                  <p className=" text-gray-500 text-sm ">
+                    {project.price} <span></span> {project.location}
+                  </p>
+                </div>
+              </div>
             </div>
           ))}
         </div>
